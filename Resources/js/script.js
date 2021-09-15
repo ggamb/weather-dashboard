3a29251dafaab52760c22d6d6be1667ae846a370
ggamb <glenngambardella372@gmail.com>
2021-09-15 15:28:15 -0400

    added divs for searched cities

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -1,4 +1,5 @@
 var currentDateEl = document.getElementById("current-date");
+var cityContainEl = document.getElementById("city-contain");
 var currentCityEl = document.getElementById("current-city");
 var currentTempEl = document.getElementById("current-temp");
 var currentWindEl = document.getElementById("current-wind");
@@ -18,11 +19,6 @@ function fetchWeather() {
         return response.json();
     })
     .then(function(response) {
-
-        console.log("original", response);
-
-        console.log("epoch", response.dt);
-
        var currentDate = moment().format("dddd, MMMM Do, YYYY");
 
         currentDateEl.textContent = currentDate;
@@ -30,6 +26,11 @@ function fetchWeather() {
         currentTempEl.textContent = "Temperature: " + Math.round(response.main.temp) + " °F";
         currentWindEl.textContent = "Wind: " + Math.round(response.wind.speed) + " MPH";
         currentHumidEl.textContent = "Humidity: " + response.main.humidity;
+
+        var newDay = document.createElement("div");
+        newDay.className = "city-contain";
+        newDay.textContent = response.name;
+        cityContainEl.appendChild(newDay);
         
         var uvKey = "https://api.openweathermap.org/data/2.5/onecall?lat=" + response.coord.lat + "&lon=" + response.coord.lon + "&units=imperial&appid=96c1c6d5967a45d7c06f700d5e294417";
         return fetch(uvKey)
@@ -59,23 +60,27 @@ function fetchWeather() {
         currentUVEl.textContent = "UV Index: ";
         currentUVEl.appendChild(uvIndexEl);
         
+        
         dayContainEl.innerHTML = "";
 
-        //Add additional information here
         for(var i = 1; i < 6; i++) {
-            
-            console.log(responseUV.daily[i].weather[0].icon);
+
              
             var dayDiv = document.createElement("div");
             var daysAway = moment().add(i, "days").format("Do, ddd");
             dayDiv.className = "col-2 day-style";
-            dayDiv.innerHTML = daysAway + "<br/><img src=https://openweathermap.org/img/wn/" + responseUV.daily[i].weather[0].icon + "@2x.png><br/>Temp: " 
-                                    + Math.round(responseUV.daily[i].temp.day) + " °F<br/>"
-                                    + "Wind: " + Math.round(responseUV.daily[i].wind_speed) + " MPH<br/>" 
-                                    + "Humidity: " + responseUV.daily[i].humidity;
+            dayDiv.innerHTML = daysAway 
+                                + "<br/><img src=https://openweathermap.org/img/wn/" + responseUV.daily[i].weather[0].icon + "@2x.png><br/>"
+                                + "Temp: " + Math.round(responseUV.daily[i].temp.day) + " °F<br/>"
+                                + "Wind: " + Math.round(responseUV.daily[i].wind_speed) + " MPH<br/>" 
+                                + "Humidity: " + responseUV.daily[i].humidity;
             dayContainEl.appendChild(dayDiv);
         }
+
+        
     })
+
+
 
     
 }
