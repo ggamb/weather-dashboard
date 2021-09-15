8dd1b282ad80e9e0130e4310f63439e1a9507872
ggamb <glenngambardella372@gmail.com>
2021-09-14 20:45:32 -0400

    added text based searching

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -3,11 +3,15 @@ var currentTempEl = document.getElementById("current-temp");
 var currentWindEl = document.getElementById("current-wind");
 var currentHumidEl = document.getElementById("current-humid");
 var currentUVEl = document.getElementById("current-uv");
+var searchButtonEl = document.getElementById("search-button");
+
 
 function fetchWeather() {
-    var apiKey = "https://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=96c1c6d5967a45d7c06f700d5e294417";
-    
-    fetch(apiKey)
+    var searchValue = document.getElementById("search-value").value;
+    var cityKey = "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&units=imperial&appid=96c1c6d5967a45d7c06f700d5e294417";
+    console.log(searchValue);
+
+    fetch(cityKey)
     .then(function(response) {
         return response.json();
     })
@@ -17,8 +21,8 @@ function fetchWeather() {
         
 
         currentCityEl.textContent = "City: " + response.name;
-        currentTempEl.textContent = "Temperature: " + response.main.temp;
-        currentWindEl.textContent = "Wind: " + response.wind.speed;
+        currentTempEl.textContent = "Temperature: " + response.main.temp + " °F";
+        currentWindEl.textContent = "Wind: " + response.wind.speed + " MPH";
         currentHumidEl.textContent = "Humidity: " + response.main.humidity;
 
 
@@ -34,6 +38,9 @@ function fetchWeather() {
         console.log("humid", responseUV);
         currentUVEl.textContent = "UV Index: " + responseUV.current.uvi;
     })
+
+    
 }
 
-fetchWeather();
+searchButtonEl.addEventListener("click", fetchWeather);
+
